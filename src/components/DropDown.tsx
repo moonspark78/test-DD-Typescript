363959a2fda14ef39bc5a37898f484eac23347e8
moonspark78 <115048076+moonspark78@users.noreply.github.com>
2024-09-23 22:15:08 +0200

    :wrench: button  :wrench:

--- src/components/DropDown.tsx
+++ src/components/DropDown.tsx
@@ -1,9 +1,20 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const DropDown = () => {
+    const [isOpen, setIsOpen] = useState(false)
   return (
     <div>
-        <button className='rounded text-white px-2 py-1 hover:bg-gray-400 bg-gray-500  text-xl'>DropDown</button>
+        <button
+            onClick={() => setIsOpen(true)}
+            className='rounded text-white px-2 py-1 hover:bg-gray-400 bg-gray-500  text-xl'
+        >
+            DropDown
+        </button>
+
+        {
+            isOpen && 
+            <div className='bg-black rounded border text-white'>Test</div>
+        }
     </div>
   )
 }
